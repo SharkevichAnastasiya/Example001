f2a3f36952362522e873913c7d659f6129983a87
SharkevichAnastasiya <sharkevich94000@gmail.com>
2023-01-27 15:42:06 +0300

    k

---
+++ Example005/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("ВВЕДИТЕ ИМЯ ПОЛЬЗОВАТЕЛЯ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("yes the Masha");
+}
+else
+{
+    Console.Write("hi");
+    Console.WriteLine(username);
+}
